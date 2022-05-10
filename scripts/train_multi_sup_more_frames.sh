ceddb4b0e3cd82e4665675f951dc650245d7952d
gzy_on_sever <942568052@qq.com>
2022-05-11 01:10:07 +0800

    new week 11

--- scripts/train_multi_sup_more_frames.sh
+++ scripts/train_multi_sup_more_frames.sh
@@ -1,24 +1,27 @@
-# This script is to train the model with multi depth, with pose supervised, and with semantic masks.
+# This script is to train the model with multi depth, with pose supervised.
 
 export CUDA_VISIBLE_DEVICES=1
 
+# DATA_PATH=~/dataset/kitti_raw_pose/dataset/
 DATA_PATH=~/dataset/KITTI_RAW/
 
 LOG_PATH=~/checkpoint/
-MODEL_NAME=kitti_raw_30_0_multi_sup_semantic_m2_0
-DATASET=kitti_raw_pose_semantic
+MODEL_NAME=kitti_raw_20_0_multi_sup_new_7_frames
+DATASET=kitti_raw_pose
 
+# SPLIT=kitti_raw_pose
 SPLIT=eigen_zhou
 
-EPOCHS=30
+EPOCHS=20
 STEP_SIZE=15
 FREEZE_EPOCHS=15
 SAVE_FREQUENCY=1
-BATCH_SIZE=8
+BATCH_SIZE=4
+FRAME_IDS="0 -1 1 -2 2 -3 3"
+NUM_MATCHING_FRAMES=3
 
 SUP_EPOCHS=0
 POSE_WEIGHT=0.05
-
 
 python -m monoscaledepth.train \
    --data_path $DATA_PATH \
@@ -34,4 +37,5 @@ python -m monoscaledepth.train \
    --add_pose_supervise \
    --begin_supervise_epoch $SUP_EPOCHS \
    --pose_weight $POSE_WEIGHT \
-   --use_semantic \
+   --frame_ids $FRAME_IDS \
+   --num_matching_frames $NUM_MATCHING_FRAMES \
